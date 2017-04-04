18694c8d1ed23ad9a8f4ba5330dc1e34e6153fe8
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-04-04 11:22:55 +0100

    changes made

--- database-views-script.sql
+++ database-views-script.sql
@@ -3,12 +3,12 @@ use sequelmovie;
 create view movie_v_trailer as
   select movieTitle, trailerURL
   from movie, trailer
-  where movieID = movieID;
+  where movieID = t_movieID;
 
 create view movie_v_trailer_poster as
   select movieTitle, trailerURL, posterLink
   from movie, trailer, poster
-  where movieID = movieID and movieID = movieID;
+  where movieID = t_movieID and movieID = p_movieID;
 
 create view movie_v_soundtrack as
   select movieTitle, soundtrackName
